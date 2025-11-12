2d2ded5507a8ca53dcc6e37ba237a8d0f183d0cd
不知火 Shiranui <supersonic@livemail.tw>
2025-11-12 07:29:33 +0000

    feat(traefik): add it

--- .scripts/remove.sh
+++ .scripts/remove.sh
@@ -8,9 +8,6 @@ if [ -n "$1" ]; then
   eval "$(echo "$1" | jq -r '.env | to_entries[] | "export \(.key)=\(.value | @sh)"')"
 fi
 
-# Pull the latest changes
-git pull origin main
-
 # Iterate over each directory in the current directory
 for dir in */; do
   # Skip certain directories
@@ -25,23 +22,9 @@ for dir in */; do
   # Navigate into the directory
   cd "$dir"
 
-  # Run init.sh if needed and track its state
-  if [ -f "init.sh" ] && { [ ! -f ".initialized" ] || ! md5sum -c .initialized >/dev/null 2>&1; }; then
-    echo "Running init.sh in $dir"
-    bash init.sh
-    md5sum init.sh >.initialized
-  fi
-
-  # Generate config.env file from template using envsubst
-  if [ -f "config.env.tmpl" ]; then
-    envsubst <config.env.tmpl >config.env
-  else
-    echo "No environment template found in $dir"
-  fi
-
   # Deploy using docker-compose if the file exists
   if [ -f "compose.yml" ]; then
-    docker-compose up -d --remove-orphans --build
+    docker-compose down
   fi
  
   # Navigate back to the parent directory
