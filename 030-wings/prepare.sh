f3087a2ad6d3b4f105bc9b9c082dd3b5c42859a7
不知火 Shiranui <supersonic@livemail.tw>
2025-11-12 10:22:37 +0000

    feat(wings): update prepare.sh

--- 030-wings/prepare.sh
+++ 030-wings/prepare.sh
@@ -2,4 +2,16 @@
 
 set -e
 
-git clone https://github.com/web-tech-tw/wings.git autogen_wings
+GIT_URL="https://github.com/web-tech-tw/wings.git"
+GEN_DIR="autogen_wings"
+
+if [ ! -d "$GEN_DIR" ]; then
+  git clone "$GIT_URL" "$GEN_DIR"
+else
+  (
+    cd "$GEN_DIR"
+    git fetch origin
+    git reset --hard "origin/main"
+    git clean -fdx
+  )
+fi
